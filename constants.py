564ef8f3ebb59283bb007b7b44fb5db4e255a9bb
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2024-09-15 12:26:57 +0330

    some little changes

--- constants.py
+++ constants.py
@@ -3,9 +3,7 @@ import enum
 
 class AudioQualities(str, enum.Enum):
     q64k_bit = "64k"
-    # _96k_bit = "96k"
     q128k_bit = "128k"
-    # _160k_bit = "160k"
     q256k_bit = "256k"
     q320k_bit = "320k"
 
@@ -13,9 +11,8 @@ class AudioQualities(str, enum.Enum):
     def directories(cls):
         return [
             cls.q64k_bit,
-            # cls._96k_bit,
             cls.q128k_bit,
-            # cls._160k_bit,
             cls.q256k_bit,
             cls.q320k_bit,
         ]
+
